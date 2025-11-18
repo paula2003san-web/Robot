aaf68761d3fb58c4ffdf040ba34f42892a4c8610
Uno <paula2003san@gmail.com>
2025-11-18 12:37:16 +0100

    Realización de clase orientación.

---
+++ src/main/java/org/iesalandalus/programacion/robot/modelo/Orientacion.java
@@ -0,0 +1,21 @@
+package org.iesalandalus.programacion.robot.modelo;
+
+public enum Orientacion {
+
+    NORTE("Norte"),
+    NORESTE("Noreste"),
+    ESTE("Este"),
+    SURESTE("Sureste"),
+    SUR("Sur"),
+    SUROESTE("Suroeste"),
+    OESTE("Oeste"),
+    NOROESTE("Noroeste");
+
+    private String nombre;
+    private Orientacion (String nombre){
+        this.nombre = nombre;
+    }
+    public String toString (){
+        return nombre;
+    }
+}
